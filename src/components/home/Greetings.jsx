5a55ca1769f1f9e7ade80f54eb0e0d824c6cd41c
gemtunde <gemtunde@gmail.com>
2025-03-19 17:42:45 +0100

    home components

---
+++ src/components/home/Greetings.jsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from "react";
+
+const Greetings = () => {
+  const [dateTime, setDateTime] = useState(new Date());
+  useEffect(() => {
+    const timer = setInterval(() => setDateTime(new Date()), 1000);
+    return () => clearInterval(timer);
+  }, []);
+
+  const formateDate = (date) => {
+    const months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    return `${months[date.getMonth()]} ${String(date.getDate()).padStart(
+      2,
+      "0"
+    )}
+        ${date.getFullYear()} `;
+  };
+
+  const formateTime = (date) =>
+    `${String(date.getHours()).padStart(2, "0")}:${String(
+      date.getMinutes()
+    ).padStart(2, "0")}:${String(date.getSeconds()).padStart(2, "0")}  `;
+  return (
+    <div className="flex items-center justify-between px-8 mt-5">
+      <div>
+        <h1 className="text-[#f5f5f5] text-2xl font-semibold tracking-wide">
+          Good Morning, Tunde
+        </h1>
+        <p className="text-[#ababab] text-sm">Give your best customers</p>
+      </div>
+      <div className="">
+        <h1 className="text-[#f5f5f5] text-2xl font-bold tracking-wide w-[130px]">
+          {formateTime(dateTime)}
+        </h1>
+        <p className="text-[#ababab] text-sm">{formateDate(dateTime)}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Greetings;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,32 @@
 import React from "react";
+import Greetings from "../components/home/Greetings";
+import MiniCard from "../components/home/MiniCard";
+import RecentOrders from "../components/home/RecentOrders";
+import { BsCashCoin } from "react-icons/bs";
+import { GrInProgress } from "react-icons/gr";
 
 const Home = () => {
   return (
     <section className="flex gap-3 bg-[#1f1f1f] h-[calc(100vh-5rem)] overflow-hidden">
       {/* left div */}
-      <div className="flex-[3] bg-red-600"></div>
+      <div className="flex-[3]">
+        <Greetings />
+        <div className="flex items-center w-full gap-3 px-8 mt-8">
+          <MiniCard
+            title="Total Earnings"
+            icon={<BsCashCoin />}
+            number={345}
+            footerNum={3.4}
+          />
+          <MiniCard
+            title="In Progress"
+            icon={<GrInProgress />}
+            number={109}
+            footerNum={5.14}
+          />
+        </div>
+        <RecentOrders />
+      </div>
       {/* right div */}
       <div className="flex-[2] bg-blue-600"></div>
     </section>
